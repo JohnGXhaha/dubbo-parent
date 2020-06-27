cb47431b279d95cd4f2b96187e713f8284d3f347
gaoxiang <gaoxiang@troila.com>
2020-06-27 16:08:21 +0800

    dubbo练习

---
+++ dubbo-client/src/main/java/com/gx/client/controller/PeopleController.java
@@ -0,0 +1,21 @@
+package com.gx.client.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.gx.common.model.People;
+import com.gx.common.service.PeopleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PeopleController {
+    @Reference
+    private PeopleService peopleService;
+    @GetMapping("/people/{name}")
+    public People getPeople(@PathVariable("name") String name) {
+        People people = new People();
+        people.setName(name);
+        return peopleService.detail(people);
+    }
+}
+
