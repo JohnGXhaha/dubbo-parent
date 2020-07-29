137d097df389ad2245f9a3cdb54c77515f84843e
gaoxiang <gaoxiang@troila.com>
2020-07-29 22:16:36 +0800

    dubbo练习

--- dubbo-server/src/main/java/com/gx/server/DubboServerApplication.java
+++ dubbo-server/src/main/java/com/gx/server/DubboServerApplication.java
@@ -1,11 +1,13 @@
 package com.gx.server;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author 37641
  */
+@EnableDubbo
 @SpringBootApplication
 public class DubboServerApplication {
 
